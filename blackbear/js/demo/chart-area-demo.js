866cad9c46af5a87dafc6f03f6a91be0e4270465
Sara Feraca <sara.feraca@duke.edu>
2019-06-27 13:58:44 -0700

    chart changes

--- blackbear/js/demo/chart-area-demo.js
+++ blackbear/js/demo/chart-area-demo.js
@@ -32,9 +32,9 @@ var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    labels: ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"],
     datasets: [{
-      label: "Earnings",
+      label: "Minutes",
       lineTension: 0.3,
       backgroundColor: "rgba(78, 115, 223, 0.05)",
       borderColor: "rgba(78, 115, 223, 1)",
@@ -46,7 +46,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgba(78, 115, 223, 1)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: [0, 10000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 40000],
+      data: [10, 20, 17, 35, 23, 25, 8],
     }],
   },
   options: {
@@ -76,10 +76,15 @@ var myLineChart = new Chart(ctx, {
         ticks: {
           maxTicksLimit: 5,
           padding: 10,
+            
           // Include a dollar sign in the ticks
           callback: function(value, index, values) {
-            return '$' + number_format(value);
-          }
+            return '' + number_format(value);
+          },
+        scaleLabel: {
+        display: true,
+        labelString: 'minutes'
+      }
         },
         gridLines: {
           color: "rgb(234, 236, 244)",
@@ -110,7 +115,7 @@ var myLineChart = new Chart(ctx, {
       callbacks: {
         label: function(tooltipItem, chart) {
           var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-          return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
+          return datasetLabel + ': ' + number_format(tooltipItem.yLabel);
         }
       }
     }
